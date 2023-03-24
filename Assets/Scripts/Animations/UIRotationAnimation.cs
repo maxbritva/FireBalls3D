2a663085f93d27e416c7f9e5af3f941023c06ada
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-24 15:42:27 +0300

    Created UI Rotation Animation

---
+++ Assets/Scripts/Animations/UIRotationAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animations
+{
+	public class UIRotationAnimation : MonoBehaviour
+	{
+		[SerializeField] [Min(0.0f)] private float _speedRotation; 
+		private void Update() => 
+			Rotate(_speedRotation);
+
+		private void Rotate(float speed)
+		{
+			transform.Rotate(Vector3.forward *-1, speed * Time.deltaTime);
+		}
+	}
+}
