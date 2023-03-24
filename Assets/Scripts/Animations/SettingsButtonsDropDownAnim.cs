0bd66b29bc7585ecd8651c55359f63c58e06f421
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-24 19:12:46 +0300

    Created Settings Button Animations

---
+++ Assets/Scripts/Animations/SettingsButtonsDropDownAnim.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DG.Tweening;
+using TweenStructures;
+using UnityEngine;
+
+namespace Animations
+{
+	public class SettingsButtonsDropDownAnim : MonoBehaviour
+	{
+		[SerializeField] private Vector2RangedTweenData _buttonsTweenData;
+		[SerializeField] private GameObject _actionRoot;
+		[SerializeField] private float _delayBetweenButtons;
+
+		private RectTransform[] _buttonTransforms;
+		private bool _active;
+
+		private void Start()
+		{
+			_buttonTransforms = _actionRoot.GetComponentsInChildren<RectTransform>();
+			TweenButtonSizes(_active, _buttonsTweenData);
+		}
+
+		public void OnButtonClicked()
+		{
+			_active = !_active;
+			TweenButtonSizes(_active, _buttonsTweenData);
+		}
+
+		private void TweenButtonSizes(bool active, Vector2RangedTweenData tweenData)
+		{
+			float delay = 0.0f;
+			foreach (RectTransform buttonTransform in _buttonTransforms)
+			{
+				Vector2 sizeDelta = active ? _buttonsTweenData.To : tweenData.From;
+
+				buttonTransform
+					.DOSizeDelta(sizeDelta, tweenData.Duration)
+					.SetDelay(delay)
+					.SetEase(tweenData.Ease);
+
+				delay += _delayBetweenButtons;
+			}
+		}
+	}
+}
--- Assets/Scripts/TweenStructures/RangedTweenData.cs
+++ Assets/Scripts/TweenStructures/RangedTweenData.cs
@@ -2,7 +2,10 @@
 
 namespace TweenStructures
 {
+	[System.Serializable]
 	public class Vector3RangedTweenData: RangedTweenData<UnityEngine.Vector3> {}
+	
+	[System.Serializable]
 	public class Vector2RangedTweenData: RangedTweenData<UnityEngine.Vector2> {}
 	public class RangedTweenData<T> : TweenData<T>
 	{
