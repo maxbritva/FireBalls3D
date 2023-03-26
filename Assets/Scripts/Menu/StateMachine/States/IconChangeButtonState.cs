30aeb64533a95e1e82cc5c931b1946e9ce0f192d
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-26 19:13:14 +0300

    Configured URP settings

--- Assets/Scripts/Menu/StateMachine/States/IconChangeButtonState.cs
+++ Assets/Scripts/Menu/StateMachine/States/IconChangeButtonState.cs
@@ -14,7 +14,7 @@ namespace Menu.StateMachine.States
 		private Image _image;
 		private AudioSource _audioSource;
 
-		private void Start()
+		private void Awake()
 		{
 			_image = GetComponent<Image>();
 			_audioSource = GetComponent<AudioSource>();
