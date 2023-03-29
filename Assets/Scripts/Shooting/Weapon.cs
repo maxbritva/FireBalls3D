33cc5e251c3c5997f97b376337ce22c3549da642
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-29 18:25:26 +0300

    Created weapon

---
+++ Assets/Scripts/Shooting/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace Shooting
+{
+	public class Weapon
+	{
+		private readonly Transform _shootPoint;
+		private readonly Projectile _projectilePrefab;
+		private readonly float _projectileSpeed;
+
+		public Weapon(Transform shootPoint, Projectile projectilePrefab, float projectileSpeed)
+		{
+			_shootPoint = shootPoint;
+			_projectilePrefab = projectilePrefab;
+			_projectileSpeed = projectileSpeed;
+		}
+
+		public void Shoot() =>
+			UnityObject
+				.Instantiate(_projectilePrefab)
+				.Shoot(_shootPoint.position, _shootPoint.forward, _projectileSpeed);
+	}
+}
