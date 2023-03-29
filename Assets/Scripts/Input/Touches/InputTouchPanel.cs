11bd7159e8bbb0b449a6ca3ef04b95b80b74787a
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-29 17:39:33 +0300

    Created Input Touch Panel

--- Assets/Scripts/Input/Touches/InputTouchPanel.cs
+++ Assets/Scripts/Input/Touches/InputTouchPanel.cs
@@ -1,6 +1,32 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class InputTouchPanel : MonoBehaviour
+public class InputTouchPanel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-   
+   private Coroutine _holdingRoutine;
+   public event Action<Touch> Begun;
+   public event Action<Touch> Holding;
+   public event Action<Touch> Ended;
+   public void OnPointerDown(PointerEventData eventData)
+   {
+      Begun?.Invoke(new Touch());
+      _holdingRoutine = StartCoroutine(ProcessHoldingInput());
+   }
+
+   public void OnPointerUp(PointerEventData eventData)
+   {
+      Ended?.Invoke(new Touch());
+      StopCoroutine(_holdingRoutine);
+   }
+
+   private IEnumerator ProcessHoldingInput()
+   {
+      while (true)
+      {
+         Holding?.Invoke(new Touch());
+         yield return null;
+      }
+   }
 }
