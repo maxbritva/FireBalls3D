01a6f9b01d3aa020f9451ccebc428e9897097693
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-29 23:17:45 +0300

    Created Destroy Triigger

---
+++ Assets/Scripts/Triggers/DestroyTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Triggers
+{
+	public class DestroyTrigger : MonoBehaviour
+	{
+		private void OnTriggerEnter(Collider other) => 
+			Destroy(other.gameObject);
+	}
+}
