71fa0c8d59e7e725582679063622871346db2dec
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-30 20:36:16 +0300

    Created Move

--- Assets/Scripts/PlayerComponents/Player.cs
+++ Assets/Scripts/PlayerComponents/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Characters;
+using Paths;
 using Shooting;
 using UnityEngine;
 
@@ -10,16 +11,26 @@ namespace PlayerComponents
 		[SerializeField] private Character _character;
 		[SerializeField] private ShootingPreferencesSo _shootingPreferences;
 
+		[SerializeField] private Path _path;
+		[SerializeField] private MovePreferencesSo _movePreferences;
+		
+
 		private FireRate _fireRate;
 		private Weapon _weapon;
+		private PathFollowing _pathFollowing;
 
 		private void Start()
 		{
 			_weapon = _shootingPreferences.CreateWeapon(_character.ShootPoint);
 			_fireRate = _shootingPreferences.CreateFireRate();
+			_pathFollowing = new PathFollowing(this, _path, _movePreferences);
 		}
 
 		public void Shoot() => 
 			_fireRate.Shoot(_weapon);
+
+		[ContextMenu(nameof(Move))]
+		public void Move() => 
+			_pathFollowing.MoveToNext();
 	}
 }
