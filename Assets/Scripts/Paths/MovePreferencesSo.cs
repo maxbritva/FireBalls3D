1281e7c61b66d4732aa906233a37552c83511ddb
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-03-30 20:29:21 +0300

    Created Pathfollowing

---
+++ Assets/Scripts/Paths/MovePreferencesSo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Paths
+{
+	[CreateAssetMenu(fileName = "MovePreferences", menuName = "ScriptableObjects/MovePreferences/MovePreferences")]
+	public class MovePreferencesSo : ScriptableObject
+	{
+		[SerializeField][Min(0.0f)] private float _durationPerWaypoint;
+		[SerializeField][Min(0.0f)] private float _rotateDuration;
+
+		public float DurationPerWaypoint => _durationPerWaypoint;
+
+		public float RotateDuration => _rotateDuration;
+
+		public float TotalDuration => _rotateDuration + _durationPerWaypoint;
+	}
+}
---
+++ Assets/Scripts/Paths/PathFollowing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Paths
+{
+	public class PathFollowing
+	{
+		private readonly MonoBehaviour _follower;
+		private readonly Path _path;
+		private readonly MovePreferencesSo _preferences;
+		private int _pathSegmentIndex;
+
+		public PathFollowing(MonoBehaviour follower, Path path, MovePreferencesSo preferences)
+		{
+			_follower = follower;
+			_path = path;
+			_preferences = preferences;
+		}
+
+		
+		public void MoveToNext()
+		{
+			if(_pathSegmentIndex >=_path.Segments.Count)
+				return;
+			PathSegment segment = _path.Segments[_pathSegmentIndex];
+			Transform[] waypoints = segment.WayPoints;
+
+			_follower.StartCoroutine(MoveBetween(waypoints));
+		}
+
+		private IEnumerator MoveBetween(IReadOnlyList<Transform> waypoints)
+		{
+			int index = 1;
+			 Transform followerTransform = _follower.transform;
+			while (index < waypoints.Count)
+			{
+				Vector3 position = waypoints[index].position;
+
+				followerTransform
+					.DOLookAt(position, _preferences.RotateDuration)
+					.OnComplete(() =>
+						followerTransform
+							.DOMove(position, _preferences.DurationPerWaypoint));
+
+				yield return new WaitForSeconds(_preferences.TotalDuration);
+				index++;
+			}
+
+			_pathSegmentIndex++;
+		}
+	}
+}
