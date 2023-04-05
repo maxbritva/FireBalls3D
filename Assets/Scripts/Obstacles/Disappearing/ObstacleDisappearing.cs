429fa62da2a038727e9524cb9edcc83bbfe67bf7
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-04-05 12:27:46 +0300

    Started creating opstacles building

---
+++ Assets/Scripts/Obstacles/Disappearing/ObstacleDisappearing.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Obstacles.Disappearing
+{
+	public class ObstacleDisappearing
+	{
+		private readonly Transform _obstaclesRoot;
+		private readonly IEnumerable<Obstacle> _obstacles;
+	}
+}
