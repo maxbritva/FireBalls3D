a3fa2684217139a057efb054ae548921949391de
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-04-07 22:05:53 +0300

    Created Menu platforms

---
+++ Assets/Scripts/Menu/Levels/MenuLevel.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Menu.Levels
+{
+	public class MenuLevel : MonoBehaviour
+	{
+		[SerializeField] private TextMeshPro _number;
+		[SerializeField] private Transform _markerHolder;
+
+		public Transform MarkerHolder => _markerHolder;
+
+		public void PaintNumber(Color color) => 
+			_number.color = color;
+	}
+}
---
+++ Assets/Scripts/Menu/Levels/MenuPlatformSequence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Menu.Levels
+{
+	public class MenuPlatformSequence : MonoBehaviour
+	{
+		[SerializeField] private MenuLevel[] _levels = Array.Empty<MenuLevel>();
+		[SerializeField] private AssetReferenceGameObject _levelMarker;
+	}
+}
